0278838ac0c0c070100b356c7106ae0fa350abaa
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-06-24 12:59:59 +0700

    Damn it!

--- register.go
+++ register.go
@@ -123,8 +123,8 @@ func registerSubmit(c *gin.Context) {
 
 	res, err = db.Exec("INSERT INTO `users_stats`(id, username, user_color, user_style, ranked_score_std, playcount_std, total_score_std, ranked_score_taiko, playcount_taiko, total_score_taiko, ranked_score_ctb, playcount_ctb, total_score_ctb, ranked_score_mania, playcount_mania, total_score_mania) VALUES (?, ?, 'black', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);", lid, username)
 	res, err = db.Exec("INSERT INTO `rx_stats`(id, username, user_color, user_style, ranked_score_std, playcount_std, total_score_std, ranked_score_taiko, playcount_taiko, total_score_taiko, ranked_score_ctb, playcount_ctb, total_score_ctb, ranked_score_mania, playcount_mania, total_score_mania) VALUES (?, ?, 'black', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);", lid, username)
-	res, err = db.Exec("INSERT INTO `users_rank`(userid) VALUES(%s)", lid)
-	res, err = db.Exec("INSERT INTO `rx_ranks`(userid) VALUES(%s)", lid)
+	res, err = db.Exec("INSERT INTO `users_rank`(userid) VALUES(?)", lid)
+	res, err = db.Exec("INSERT INTO `rx_rank`(userid) VALUES(?)", lid)
 	if err != nil {
 		fmt.Println(err)
 	}
